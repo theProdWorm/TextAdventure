c8570c03df1d982b415291a8a83611b8c8284b17
Emma Fast <emma@fastcode.se>
2024-09-13 20:31:09 +0200

    Floor gen rework

--- World/Floor.cs
+++ World/Floor.cs
@@ -15,8 +15,8 @@ public class Floor
     private LootFactory _lootFactory { get; }
     #endregion FACTORIES
 
-    private int _nRooms;
-    private List<Room> _rooms;
+    private int _nRoomsOnFloor;
+    private List<List<Room>> _rooms;
     private bool _generateBonusRoom;
     private int _enemyCounts;
     private Room _currentRoom;
@@ -24,14 +24,14 @@ public class Floor
 
     private Action _nextFloor;
     
-    public Floor(WeaponFactory weaponFactory, ArmorFactory armorFactory, EnemyFactory enemyFactory, LootFactory lootFactory, int rooms, int enemyCounts, Action nextFloor, bool generateBonusRoom = false)
+    public Floor(WeaponFactory weaponFactory, ArmorFactory armorFactory, EnemyFactory enemyFactory, LootFactory lootFactory, int roomsOnFloor, int enemyCounts, Action nextFloor, bool generateBonusRoom = false)
     {
         _weaponFactory = weaponFactory;
         _armorFactory = armorFactory;
         _enemyFactory = enemyFactory;
         _lootFactory = lootFactory;
         
-        _nRooms = rooms;
+        _nRoomsOnFloor = roomsOnFloor;
         _rooms = [];
         _generateBonusRoom = generateBonusRoom;
         _enemyCounts = enemyCounts;
@@ -39,59 +39,73 @@ public class Floor
         
         _roomFactory = new RoomFactory(_enemyFactory, _lootFactory, _nextFloor);
         GenerateRooms();
-        _currentRoom = _rooms.First();
+        _currentRoom = _rooms.First().First();
     }
 
     private void GenerateRooms()
     {
-        int effectiveEnemyCount = _enemyCounts;
-        _rooms.Add(_roomFactory.GenerateCombatRoom(effectiveEnemyCount));
-        for (int i = 1; i < _nRooms; i++)
-        {
-            if (i == _nRooms - 1)
-            {
-                effectiveEnemyCount++;
-            }
-            Room room1 = _roomFactory.GenerateCombatRoom(effectiveEnemyCount);
-            Room room2 = _roomFactory.GenerateCombatRoom(effectiveEnemyCount);
+        _rooms.Add([_roomFactory.GenerateCombatRoom(_enemyCounts)]);
 
-            Door door1 = new Door(room1, EnterRoom);
-            Door door2 = new Door(room2, EnterRoom);
-            
-            _rooms[^1].AddDoor(door1);
-            _rooms[^1].AddDoor(door2);
-            if (_rooms.Count > 1)
+        for (int i = 1; i < _nRoomsOnFloor; i++)
+        {
+            int roomsToGenerate = Game.random.Next(2, 4);
+            List<Room> rooms = [];
+            for (int j = 0; j < roomsToGenerate; j++)
             {
-                _rooms[^2].AddDoor(door1);
-                _rooms[^2].AddDoor(door2);
+                rooms.Add(_roomFactory.GenerateCombatRoom(_enemyCounts));
             }
-            _rooms.Add(room1);
-            _rooms.Add(room2);
+
+            foreach (Room previousRoom in _rooms[^1])
+            {
+                foreach (Room currentRoom in rooms)
+                {
+                    previousRoom.AddDoor(new Door(currentRoom, EnterRoom));
+                }
+            }
+            _rooms.Add(rooms);
         }
-        
+        List<Room> endRooms = [];
+
         Room shopRoom = _roomFactory.GenerateShopRoom();
         Door shopDoor = new Door(shopRoom, EnterRoom);
-        _rooms[^1].AddDoor(shopDoor);
-        _rooms[^2].AddDoor(shopDoor);
+        //_rooms[^1].AddDoor(shopDoor);
+        //_rooms[^2].AddDoor(shopDoor);
+        foreach (Room room in _rooms[^1])
+        {
+            room.AddDoor(shopDoor);
+        }
+        endRooms.Add(shopRoom);
 
         Room nextFloorRoom = _roomFactory.GenerateNextFloorRoom();
         Door nextFloorDoor = new Door(nextFloorRoom, EnterRoom);
-        _rooms[^1].AddDoor(nextFloorDoor);
-        _rooms[^2].AddDoor(nextFloorDoor);
+        //_rooms[^1].AddDoor(nextFloorDoor);
+        //_rooms[^2].AddDoor(nextFloorDoor);
+        foreach (Room room in _rooms[^1])
+        {
+            room.AddDoor(nextFloorDoor);
+        }
         shopRoom.AddDoor(nextFloorDoor);
 
         if (_generateBonusRoom)
         {
             Room bonusRoom = _roomFactory.GenerateBonusRoom();
             Door bonusDoor = new Door(bonusRoom, EnterRoom);
-            _rooms[^1].AddDoor(bonusDoor);
-            _rooms[^2].AddDoor(bonusDoor);
+            //_rooms[^1].AddDoor(bonusDoor);
+            //_rooms[^2].AddDoor(bonusDoor);
+            
+            foreach (Room room in _rooms[^1])
+            {
+                room.AddDoor(bonusDoor);
+            }
             shopRoom.AddDoor(bonusDoor);
             bonusRoom.AddDoor(shopDoor);
-            _rooms.Add(bonusRoom);
+            //_rooms.Add(bonusRoom);
+            endRooms.Add(bonusRoom);
         }
-        _rooms.Add(shopRoom);
-        _rooms.Add(nextFloorRoom);
+        endRooms.Add(nextFloorRoom);
+        _rooms.Add(endRooms);
+        //_rooms.Add(shopRoom);
+        //_rooms.Add(nextFloorRoom);
     }
 
     private void EnterRoom(Room room)
