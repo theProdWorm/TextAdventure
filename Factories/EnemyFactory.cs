2adbd0b04ebe79dbe2c39f21e2ae76c1370010b3
Emil Rohlen <59256884+theProdWorm@users.noreply.github.com>
2024-09-13 11:32:46 +0200

    resolve conflict

--- Factories/EnemyFactory.cs
+++ Factories/EnemyFactory.cs
@@ -1,23 +1,19 @@
 using TextAdventure.Characters;
+using TextAdventure.Items.Armors;
+using TextAdventure.Items.Weapons;
 
 namespace TextAdventure.Factories;
 
-public class EnemyFactory : Factory
+public class EnemyFactory(WeaponFactory weaponFactory, ArmorFactory armorFactory) : Factory
 {
-    private WeaponFactory _weaponFactory;
-    private ArmorFactory _armorFactory;
+    private readonly WeaponFactory _weaponFactory = weaponFactory;
+    private readonly ArmorFactory _armorFactory = armorFactory;
     
     private readonly Dictionary<string, WeightedElement<(string, int)>> _enemyTypes = new();
 
-    public EnemyFactory(WeaponFactory weaponFactory, ArmorFactory armorFactory)
+    public void RegisterEnemyType(int weight, (string, int) nameAndHealth)
     {
-        _weaponFactory = weaponFactory;
-        _armorFactory = armorFactory;
-    }
-
-    public void RegisterEnemyType(string key, int weight, (string, int) enemyValues)
-    {
-        _enemyTypes.Add(key, new WeightedElement<(string, int)>(weight, enemyValues));
+        _enemyTypes.Add(nameAndHealth.Item1, new WeightedElement<(string, int)>(weight, nameAndHealth));
     }
 
     public Character GenerateEnemy()
@@ -26,6 +22,7 @@ public class EnemyFactory : Factory
         Character enemy = new Character(values.Item1, values.Item2);
         enemy.Equip(_weaponFactory.GenerateWeapon());
         enemy.Equip(_armorFactory.GenerateArmor());
+
         return enemy;
     }
 }
